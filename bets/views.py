3c0f2f86dd0ff15f02d0544595a8dd626bb8872c
George <gpanterov@g-computer.(none)>
2015-06-15 20:09:23 -0400

    Initiate user balance during registration

--- bets/views.py
+++ bets/views.py
@@ -48,50 +48,50 @@ def usdjpy(request):
 
 
 def register(request):
-    # Like before, get the request's context.
-    context = RequestContext(request)
+	# Like before, get the request's context.
+	context = RequestContext(request)
 
-    # A boolean value for telling the template whether the registration was successful.
-    # Set to False initially. Code changes value to True when registration succeeds.
-    registered = False
-    password = None
-    # If it's a HTTP POST, we're interested in processing form data.
-    if request.method == 'POST':
-        # Attempt to grab information from the raw form information.
-        # Note that we make use of both UserForm and UserProfileForm.
-        user_form = UserForm(data=request.POST)
+	# A boolean value for telling the template whether the registration was successful.
+	# Set to False initially. Code changes value to True when registration succeeds.
+	registered = False
+	password = None
+	# If it's a HTTP POST, we're interested in processing form data.
+	if request.method == 'POST':
+		# Attempt to grab information from the raw form information.
+		# Note that we make use of both UserForm and UserProfileForm.
+		user_form = UserForm(data=request.POST)
+		# If the two forms are valid...
+		if user_form.is_valid():
+		# Save the user's form data to the database.
 
-        # If the two forms are valid...
-        if user_form.is_valid():
-            # Save the user's form data to the database.
-            user = user_form.save()
-            # Now we hash the password with the set_password method.
-            # Once hashed, we can update the user object.
-            user.set_password(user.password)
+			user = user_form.save()
+			# Now we hash the password with the set_password method.
+			# Once hashed, we can update the user object.
+			user.set_password(user.password)
+			user.save()
 
-            user.save()
+			
 
+			# Update our variable to tell the template registration was successful.
+			registered = True
+			# Create entry for the user's balance
+			bal = Balances()
+			bal.username = user.username
+			bal.balance = 0
+			bal.save()
+		# Invalid form or forms - mistakes or something else?
+		# Print problems to the terminal.
+		# They'll also be shown to the user.
+		else:
+			print user_form.errors
+		# Not a HTTP POST, so we render our form using two ModelForm instances.
+		# These forms will be blank, ready for user input.
+	else:
+		user_form = UserForm()
 
-
-            # Update our variable to tell the template registration was successful.
-            registered = True
-
-        # Invalid form or forms - mistakes or something else?
-        # Print problems to the terminal.
-        # They'll also be shown to the user.
-        else:
-            print user_form.errors
-
-    # Not a HTTP POST, so we render our form using two ModelForm instances.
-    # These forms will be blank, ready for user input.
-    else:
-        user_form = UserForm()
-
-    # Render the template depending on the context.
-    return render_to_response(
-            'bets/new_register.html',
-            {'user_form': user_form, 'registered': registered},
-            context)
+	# Render the template depending on the context.
+	return render_to_response('bets/new_register.html',
+	{'registered': registered},	context)
 
 
 def user_login(request):
