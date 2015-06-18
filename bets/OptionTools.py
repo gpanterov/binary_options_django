60111d377a9edc2666b76f8fc21e923a956cc577
George <gpanterov@g-computer.(none)>
2015-06-18 11:49:50 -0400

    Minor changes

--- bets/OptionTools.py
+++ bets/OptionTools.py
@@ -104,7 +104,7 @@ def get_closest_prices(asset, timestamp):
 
 def send_money(to, amount):
 	guid = 'd99c232a-2ab7-4371-ad4b-ad8b8aa74008'
-	main_password=''
+	main_password='Never_the_s4am3_aga1n'
 	url = "https://blockchain.info/merchant/" + guid + "/payment?password=" + main_password + \
 			"&to=" + to + "&amount=" + amount
 
